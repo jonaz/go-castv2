342540ab5e2ff6e5c334aa7431ef1ceea5941b3c
Jonas Falck <jonas.falck@gmail.com>
2014-11-07 10:27:09 +0100

    forgot to add files

---
+++ controllers/MediaController.go
@@ -0,0 +1,87 @@
+package controllers
+
+import (
+	"encoding/json"
+	"log"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/jonaz/go-castv2"
+	"github.com/jonaz/go-castv2/api"
+)
+
+type MediaController struct {
+	interval time.Duration
+	channel  *castv2.Channel
+	Incoming chan *StatusResponse
+}
+
+func NewMediaController(client *castv2.Client, sourceId, destinationId string) *MediaController {
+	controller := &MediaController{
+		channel:  client.NewChannel(sourceId, destinationId, "urn:x-cast:com.google.cast.media"),
+		Incoming: make(chan *StatusResponse, 0),
+	}
+
+	controller.channel.OnMessage("MEDIA_STATUS", controller.onStatus)
+
+	return controller
+}
+
+func (c *MediaController) onStatus(message *api.CastMessage) {
+	spew.Dump("Got status message", message)
+
+	response := &StatusResponse{}
+
+	err := json.Unmarshal([]byte(*message.PayloadUtf8), response)
+
+	if err != nil {
+		log.Printf("Failed to unmarshal status message:%s - %s", err, *message.PayloadUtf8)
+		return
+	}
+
+	select {
+	case c.Incoming <- response:
+	default:
+		log.Printf("Incoming status, but we aren't listening. %v", response)
+	}
+
+}
+
+//type StatusResponse struct {
+//Status *ReceiverStatus `json:"status,omitempty"`
+//}
+
+//type ReceiverStatus struct {
+//castv2.PayloadHeaders
+//Volume *VolumePayload `json:"volume,omitempty"`
+//}
+
+//type VolumePayload struct {
+//Level *float64 `json:"level,omitempty"`
+//Muted *bool    `json:"muted,omitempty"`
+//}
+
+func (c *MediaController) GetStatus(timeout time.Duration) (*api.CastMessage, error) {
+	getStatus := castv2.PayloadHeaders{Type: "GET_STATUS"}
+	return c.channel.Request(&getStatus, timeout)
+}
+
+//func (c *ReceiverController) SetVolume(volume *VolumePayload, timeout time.Duration) (*api.CastMessage, error) {
+//return c.channel.Request(&ReceiverStatus{
+//castv2.PayloadHeaders{Type: "SET_VOLUME"}, volume,
+//}, timeout)
+//}
+
+//func (c *ReceiverController) GetVolume(timeout time.Duration) (*VolumePayload, error) {
+//message, err := c.GetStatus(timeout)
+
+//if err != nil {
+//return nil, err
+//}
+
+//response := StatusResponse{}
+
+//err = json.Unmarshal([]byte(*message.PayloadUtf8), &response)
+
+//return response.Status.Volume, err
+//}
